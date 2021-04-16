bab47b277d15c5c5f82aa4476d52aac849df36fa
yuelinn <yuelinnchong@gmail.com>
2021-04-17 03:14:15 +0800

    adding python3 template

---
+++ python3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+'''
+@script description
+
+'''
+
+# builtin 
+
+# third-party
+
+# own mods
+
+__author__ = "Linn of the Ded"
+__status__ = "Production"
+
+if __name__ == '__main__':
+	pass
