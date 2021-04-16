bc87133384014c59e49f94868c6cc05befc9a7a2
yuelinn <yuelinnchong@gmail.com>
2021-04-17 03:13:20 +0800

    adding python2 template

---
+++ python2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+'''
+@script description
+
+'''
+
+# builtin 
+from __future__ import absolute_import, division, print_function # for python2 and python3 compatibility
+
+# third-party
+
+# own mods
+
+__author__ = "Linn of the Ded"
+__status__ = "Production"
+
+if __name__ == '__main__':
+	pass
