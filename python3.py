381d23a6cb101a2205c97a7633e7adc245bdc19b
Chong Yue Linn <yuelinnchong@gmail.com>
2021-11-17 19:53:29 +0100

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+"""A one line summary of the module or program, terminated by a period.
 
-'''
-@script description
+Leave one blank line.  The rest of this docstring should contain an
+overall description of the module or program.  Optionally, it may also
+contain a brief description of exported classes and functions and/or usage
+examples.
 
-'''
+  Typical usage example:
+
+  foo = ClassFoo()
+  bar = foo.FunctionBar()
+"""
 
 # builtin 
 
@@ -13,6 +20,33 @@
 
 __author__ = "Linn of the Ded"
 __status__ = "Production"
+
+class Class_template:
+    """Summary of class here.
+
+    Longer class information....
+    Longer class information....
+
+    Attributes:
+        likes_spam: A boolean indicating if we like SPAM or not.
+        eggs: An integer count of the eggs we have laid.
+    """
+    
+    def func(a1):
+        """Documentation for a function.
+        
+        More detailed description of function.
+        
+        Args:
+          a1: something about the input.
+
+        Returns:
+          The output description.
+
+        Raises:
+          ErrorType: When the error will be raised.
+        """
+        pass
 
 if __name__ == '__main__':
     pass
